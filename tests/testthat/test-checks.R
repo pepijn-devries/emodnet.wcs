79d12bcc6037449b9479b736abbaece1227fbade
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-07-04 14:17:31 +0200

    chore: format (Air + empty lines)

--- tests/testthat/test-checks.R
+++ tests/testthat/test-checks.R
@@ -3,6 +3,7 @@ test_that("validate_namespace works", {
     validate_namespace("emodnet:2018_st_All_avg_POSTER"),
     "emodnet__2018_st_All_avg_POSTER"
   )
+
   expect_equal(
     validate_namespace("emodnet__2018_st_All_avg_POSTER"),
     "emodnet__2018_st_All_avg_POSTER"
@@ -17,35 +18,43 @@ test_that("validate_bbox works", {
       xmax = 180.000000000072,
       ymax = 90
     )),
-    structure(c(
-      -180, -90.000000000036,
-      180.000000000072, 90
-    ),
-    .Dim = c(2L, 2L),
-    .Dimnames = list(
-      c("x", "y"),
-      c("min", "max")
-    )
+    structure(
+      c(
+        -180,
+        -90.000000000036,
+        180.000000000072,
+        90
+      ),
+      .Dim = c(2L, 2L),
+      .Dimnames = list(
+        c("x", "y"),
+        c("min", "max")
+      )
     )
   )
+
   expect_error(validate_bbox(c(
     xmin = -90.000000000036,
     ymin = -180,
     xmax = -180.000000000072,
     ymax = 90
   )))
+
   expect_error(validate_bbox(c(
     xmin = -90.000000000036,
     ymin = -180,
     ymax = 90
   )))
+
   expect_error(validate_bbox(c(
     xmin = -180,
     ymin = -90.000000000036,
     xmax = 180.000000000072,
     ymax = "90"
   )))
+
   expect_error(validate_bbox(NA))
+
   expect_equal(validate_bbox(NULL), NULL)
 })
 
@@ -71,16 +80,18 @@ test_that("check coverages works", {
     "Emodnetbio__cal_fin_19582016_L1_err",
     "Emodnetbio__cal_hel_19582016_L1_err"
   )
+
   expect_invisible(check_coverages(wcs, coverage_ids))
   expect_error(check_coverages(wcs, "erroneous_id"))
 })
 
 test_that("validate rangesubset works", {
-    summary <- create_biology_summary()[[1]]
-    with_mock_dir("biology-description", {
+  summary <- create_biology_summary()[[1]]
+  with_mock_dir("biology-description", {
     coverage_id <- "Emodnetbio__ratio_large_to_small_19582016_L1_err"
 
     expect_invisible(validate_rangesubset(summary, "relative_abundance"))
+
     expect_error(validate_rangesubset(summary, "erroneous_rangetype"))
-    })
+  })
 })

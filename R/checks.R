6bdb502826f3f6cdf5abf049bdfaf9ae04bb8ecd
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-09-05 11:54:06 +0200

    fix: handle sf message

--- R/checks.R
+++ R/checks.R
@@ -127,9 +127,7 @@ check_cov_contains_bbox <- function(summary, bbox, crs) {
   user_bbox <- sf::st_as_sfc(sf::st_bbox(user_bbox)) |>
     sf::st_transform(crs = sf::st_crs(cov_bbox)) # cov_bbox can be the same
 
-  intersects <- isTRUE(as.logical(sf::st_intersects(user_bbox, cov_bbox)))
-
-  if (!intersects) {
+  if (!s2_intersects(user_bbox, cov_bbox)) {
     message <-
       "{.var bbox} boundaries lie outside coverage extent.
       No overlapping data to download."
@@ -144,6 +142,13 @@ check_cov_contains_bbox <- function(summary, bbox, crs) {
     }
     cli::cli_warn(message)
   }
+}
+
+s2_intersects <- function(bbox1, bbox2) {
+  current <- sf::sf_use_s2()
+  sf::sf_use_s2(TRUE)
+  on.exit(sf::sf_use_s2(current))
+  isTRUE(as.logical(sf::st_intersects(bbox1, bbox2)))
 }
 
 # ---- validations ----
